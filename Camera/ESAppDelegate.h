f1c884c194b1a2edf4cc7f49d93aaf20746d50b0
eesierra <esierr1@gmail.com>
2013-11-05 14:53:38 -0800

    Initial Commit

---
+++ Camera/ESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESAppDelegate.h
+//  Camera
+//
+//  Created by Eduardo Sierra on 11/5/13.
+//  Copyright (c) 2013 Sierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Camera/ESViewController.h
@@ -0,0 +1,13 @@
+//
+//  ESViewController.h
+//  Camera
+//
+//  Created by Eduardo Sierra on 11/5/13.
+//  Copyright (c) 2013 Sierra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESViewController : UIViewController
+
+@end
